84db8688b8fb49d4983141657c4b996d973d5cbe
sreiswig <sam.reiswig@gmail.com>
2024-12-21 13:01:05 -0600

    env fixes

--- devenv.nix
+++ devenv.nix
@@ -18,7 +18,6 @@ in
 
   env = {
     LD_LIBRARY_PATH = "${
-      with pkgs;
       lib.makeLibraryPath buildInputs
     }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
     CUDA_PATH = pkgs.cudaPackages.cudatoolkit;
