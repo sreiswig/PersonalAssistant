615472d7162c312a44eeb010c7dc8c0a15622282
sreiswig <sam.reiswig@gmail.com>
2024-12-22 23:16:56 -0600

    prep to convert into a chat ui

--- src/gui/hi.py
+++ src/gui/hi.py
@@ -5,23 +5,9 @@ from PySide6 import QtCore, QtWidgets, QtGui
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
-        self.textEdit = QtWidgets.QTextEdit("Test")
-
+        self.textEdit = QtWidgets.QTextEdit("Talk to Transformer")
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
         self.layout.addWidget(self.textEdit)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
