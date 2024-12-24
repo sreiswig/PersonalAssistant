b71e9ae127fd776918e380e76a13ff35de9e9567
sreiswig <sam.reiswig@gmail.com>
2024-12-24 10:44:30 -0600

    loop voice

--- src/main.py
+++ src/main.py
@@ -13,13 +13,16 @@ def main():
     v2t = VoiceToTextModel(config["voicetotext"])
     llm = TextToTextModel(config["texttotext"])
     t2v = TextToVoiceModel(config["texttovoice"])
-    results = m.listen()
-    results = v2t.run(results)
-    print(results)
-    results = llm.run(results["text"])
-    print(results)
-    results = t2v.run(results)
-    m.speak(results)
+    while True:
+        results = m.listen()
+        user_input = v2t.run(results)
+        print(user_input)
+        if user_input["text"].strip().lower() == "stop listening.":
+            break
+        results = llm.run(user_input["text"])
+        print(results)
+        results = t2v.run(results)
+        m.speak(results)
     m.close()
 
 if __name__ == "__main__":
