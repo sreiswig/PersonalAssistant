ce7d03a824c2dd0970cc635622f29808e41d129b
sreiswig <sam.reiswig@gmail.com>
2025-02-02 23:03:48 -0600

    split the config

--- src/client/main.py
+++ src/client/main.py
@@ -1,6 +1,7 @@
 import tomllib
 from microphone import Microphone
-from models import VoiceToTextModel, TextToTextModel, TextToVoiceModel
+from voice_to_text_model import VoiceToTextModel 
+from text_to_voice_model import TextToVoiceModel
 
 def get_config():
     with open("pyproject.toml", "rb") as f:
@@ -11,16 +12,15 @@ def main():
     print(config["microphone"])
     m = Microphone(config["microphone"])
     v2t = VoiceToTextModel(config["voicetotext"])
-    llm = TextToTextModel(config["texttotext"])
+    # Setup connection to the server
     t2v = TextToVoiceModel(config["texttovoice"])
     while True:
         results = m.listen()
-        user_input = v2t.run(results)
-        print(user_input)
-        if user_input["text"].strip().lower() == "stop listening.":
-            break
-        results = llm.run(user_input["text"])
+        results = v2t.run(results)
         print(results)
+        if results["text"].strip().lower() == "stop listening.":
+            break
+        # Call the server and wait for results
         results = t2v.run(results)
         m.speak(results)
     m.close()
