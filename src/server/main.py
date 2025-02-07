e25651c258732f5787312aa71741f652db5df665
sreiswig <sam.reiswig@gmail.com>
2025-02-07 16:52:55 -0600

    lifespan fastapi

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,23 @@
 from typing import Union
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-app = FastAPI()
+models = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    models["text"] = None
+    yield
+    models.clear()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
     return {"Hello", "AIServer"}
+
+@app.get("/predict")
+async def predict(x: float):
+    result = models["text"](x)
+    return {"result": result}
