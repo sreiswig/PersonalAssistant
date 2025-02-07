e048691a821a06e616656f300e1ed98ed98a5985
sreiswig <sam.reiswig@gmail.com>
2025-02-06 20:08:37 -0600

    adds test agent

--- src/server/test_agent.py
+++ src/server/test_agent.py
@@ -1,11 +1,44 @@
 from typing import Annotated
 
+from langchain_core.tools import tool
 from typing_extensions import TypedDict
 
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from langgraph.types import Command, interrupt
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 graph_builder = StateGraph(State)
+
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human"""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool, human_assistance]
+llm = None
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    message = llm_with_tools.invoke(state["messages"])
+    assert len(message.tool_calls) <= 1
+    return {"messages": [message]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+        "chatbot",
+        tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
