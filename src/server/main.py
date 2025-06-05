e1a7465add1eac48235bd795cb10d92351c797b6
sreiswig <sam.reiswig@gmail.com>
2025-06-05 00:23:10 -0500

    model wrapper class

--- src/server/main.py
+++ src/server/main.py
@@ -1,13 +1,16 @@
 import tomllib
 
 from contextlib import asynccontextmanager
+from typing import List
 from .models import GoogleAIStudioModel
 from .models import HuggingFaceModel
 from .config import ServerSettings
+from .models import ModelMenu
 
 import uvicorn
 from fastapi import FastAPI
 
+available_models = ["DistilBert"]
 model = {}
 
 
@@ -27,6 +30,9 @@ app = FastAPI(lifespan=lifespan)
 def read_root():
     return {"Hello AIServer"}
 
+@app.get("/model_menu", response_model=ModelMenu)
+async def get_available_models():
+    return available_models
 
 @app.post("/predict")
 async def predict(x: str):
--- src/server/models/__init__.py
+++ src/server/models/__init__.py
@@ -1,2 +1,3 @@
 from .huggingface_models import HuggingFaceModel
 from .google_ai_studio_models import GoogleAIStudioModel
+from .model_menu import ModelMenu
